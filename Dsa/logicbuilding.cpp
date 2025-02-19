35e431dc149cd8010995ffbcf64045cd6c3f3cf8
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2025-02-19 22:39:16 +0530

    Update logicbuilding.cpp

--- Dsa/logicbuilding.cpp
+++ Dsa/logicbuilding.cpp
@@ -11,7 +11,7 @@
 // int main(){
 //     int num;
 //     cin >> num;
-//     if(num % 2 == 0){
+//     if(num % 2 == 0){    
 //         cout << num << " is even" << endl;
 //     }else{
 //         cout<< num << " is odd" << endl;
